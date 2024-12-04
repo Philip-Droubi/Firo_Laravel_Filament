89e7475b2afa14bd2c0001a5f9f7fa29768b3194
Philip Droubi <philipdroubi@gmail.com>
2024-12-04 20:53:31 +0300

    User Service Table & Filters

--- app/Filament/Resources/Users/Service/UserServiceResource/Pages/ListUserServices.php
+++ app/Filament/Resources/Users/Service/UserServiceResource/Pages/ListUserServices.php
@@ -13,7 +13,7 @@ class ListUserServices extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //
         ];
     }
 }
